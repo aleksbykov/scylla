1ed2a40087b332d9bcb9f0d671d4cfa8748f8724
Aleksandr Bykov <alex.bykov@scylladb.com>
2024-04-26 02:16:07 +0700

    fix(abort-topology-operation): Abort topology operations to repo 17612
    put old code which should reproduce issue 17612 on arm

--- test/topology_experimental_raft/test_crash_coordinator_before_streaming.py
+++ test/topology_experimental_raft/test_crash_coordinator_before_streaming.py
@@ -11,7 +11,6 @@ import pytest
 
 from test.pylib.manager_client import ManagerClient
 from test.pylib.scylla_cluster import ReplaceConfig
-from test.topology.conftest import skip_mode
 from test.topology.util import (check_token_ring_and_group0_consistency, wait_for_token_ring_and_group0_consistency,
                                 get_coordinator_host, get_coordinator_host_ids, wait_new_coordinator_elected)
 
@@ -20,7 +19,6 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.asyncio
-@skip_mode('release', 'error injections are not supported in release mode')
 async def test_kill_coordinator_during_op(manager: ManagerClient) -> None:
     """ Kill coordinator with error injection while topology operation is running for cluster: decommission,
     bootstrap, removenode, replace.
@@ -46,6 +44,8 @@ async def test_kill_coordinator_during_op(manager: ManagerClient) -> None:
         'failure_detector_timeout_in_ms': 2000
     }
     nodes = [await manager.server_add(config=config) for _ in range(5)]
+    cql = manager.get_cql()
+
     coordinators_ids = await get_coordinator_host_ids(manager)
     assert len(coordinators_ids) == 1, "At least 1 coordinator id should be found"
 
@@ -57,7 +57,6 @@ async def test_kill_coordinator_during_op(manager: ManagerClient) -> None:
     await manager.decommission_node(server_id=other_nodes[-1].server_id, expected_error="Decommission failed. See earlier errors")
     await wait_new_coordinator_elected(manager, 2, time.time() + 60)
     await manager.server_restart(coordinator_host.server_id, wait_others=1)
-    await manager.servers_see_each_other(await manager.running_servers())
     await check_token_ring_and_group0_consistency(manager)
 
     # kill coordinator during removenode
@@ -67,20 +66,18 @@ async def test_kill_coordinator_during_op(manager: ManagerClient) -> None:
     other_nodes = [srv for srv in nodes if srv.server_id != coordinator_host.server_id]
     working_srv_id = other_nodes[0].server_id
     node_to_remove_srv_id = other_nodes[-1].server_id
-    logger.debug("Stop node with srv_id %s", node_to_remove_srv_id)
+    logger.debug("Stop node with srv_id%s", node_to_remove_srv_id)
     await manager.server_stop_gracefully(node_to_remove_srv_id)
     await manager.api.enable_injection(coordinator_host.ip_addr, "crash_coordinator_before_stream", one_shot=True)
-    logger.debug("Start removenode with srv_id %s from node with srv_id %s", node_to_remove_srv_id, working_srv_id)
+    logger.debug("start removenode with srv_id %s from node with srv_id %s", node_to_remove_srv_id, working_srv_id)
     await manager.remove_node(working_srv_id,
                               node_to_remove_srv_id,
                               expected_error="Removenode failed. See earlier errors")
 
     await wait_new_coordinator_elected(manager, 3, time.time() + 60)
-    await manager.others_not_see_server(server_ip=coordinator_host.ip_addr)
     logger.debug("Start old coordinator node with srv_id %s", coordinator_host.server_id)
     await manager.server_restart(coordinator_host.server_id, wait_others=1)
-    await manager.servers_see_each_other(await manager.running_servers())
-    logger.debug("Remove node with srv_id %s from node with srv_id %s because it was banned in a previous attempt", node_to_remove_srv_id, working_srv_id)
+    logger.debug("Remove node with srv_id %s from node with srv_id %s because it was banned", working_srv_id, node_to_remove_srv_id)
     await manager.remove_node(working_srv_id, node_to_remove_srv_id)
     await check_token_ring_and_group0_consistency(manager)
     logger.debug("Restore number of nodes in cluster")
@@ -96,8 +93,7 @@ async def test_kill_coordinator_during_op(manager: ManagerClient) -> None:
     await manager.server_start(new_node.server_id,
                                expected_error="Startup failed: std::runtime_error")
     await wait_new_coordinator_elected(manager, 4, time.time() + 60)
-    await manager.server_restart(coordinator_host.server_id, wait_others=1)
-    await manager.servers_see_each_other(await manager.running_servers())
+    await manager.server_restart(coordinator_host.server_id)
     await check_token_ring_and_group0_consistency(manager)
 
     # kill coordinator during replace
@@ -111,13 +107,10 @@ async def test_kill_coordinator_during_op(manager: ManagerClient) -> None:
     replace_cfg = ReplaceConfig(replaced_id = node_to_replace_srv_id, reuse_ip_addr = False, use_host_id = True)
     new_node = await manager.server_add(start=False, replace_cfg=replace_cfg)
     await manager.server_start(new_node.server_id, expected_error="Replace failed. See earlier errors")
+
     await wait_new_coordinator_elected(manager, 5, time.time() + 60)
-    logger.debug("Start old coordinator node")
-    await manager.others_not_see_server(server_ip=coordinator_host.ip_addr)
+    await manager.server_restart(node_to_replace_srv_id, wait_others=1)
     await manager.server_restart(coordinator_host.server_id, wait_others=1)
-    await manager.servers_see_each_other(await manager.running_servers())
-    logger.debug("Replaced node is already non-voter and will be banned after restart. Remove it")
-    coordinator_host = await get_coordinator_host(manager)
-    await manager.remove_node(coordinator_host.server_id, node_to_replace_srv_id)
+
     await wait_for_token_ring_and_group0_consistency(manager, time.time() + 60)
     await check_token_ring_and_group0_consistency(manager)
